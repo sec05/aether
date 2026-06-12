af03d276dd08c1ce741b55a59205c0387b5dd46e
sec05 <spencer.ec09@gmail.com>
2026-06-11 21:38:58 -0400

    Basic test to check Eigen install

--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -1,6 +1,7 @@
 #include <type_traits>
 #include <variant>
 
+#include <Eigen/Dense>
 #include <gtest/gtest.h>
 
 #include "aether/core/types.hpp"
@@ -13,4 +14,19 @@ TEST(Conventions, UsesConfiguredTypes) {
   static_assert(std::is_same_v<aether::Numeric<>::ScalarType, double>);
   static_assert(std::is_same_v<aether::Numeric<float>::ScalarType, float>);
   static_assert(std::is_same_v<aether::Expected<int, int>, std::variant<int, int>>);
+}
+
+TEST(Eigen, MatrixMultiplicationWorks) {
+  Eigen::Matrix2d left;
+  left << 1.0, 2.0, 3.0, 4.0;
+
+  Eigen::Matrix2d right;
+  right << 5.0, 6.0, 7.0, 8.0;
+
+  const Eigen::Matrix2d product = left * right;
+
+  EXPECT_DOUBLE_EQ(product(0, 0), 19.0);
+  EXPECT_DOUBLE_EQ(product(0, 1), 22.0);
+  EXPECT_DOUBLE_EQ(product(1, 0), 43.0);
+  EXPECT_DOUBLE_EQ(product(1, 1), 50.0);
 }
